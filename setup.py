4ac6e6606c70c415f5985fae2f16183c36f51fad
kohinoor123shrestha <kohinoorshrestha0@gmail.com>
2025-01-26 18:07:21 +0545

    chatbot

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="AI-powered-chatbot",
+    version='0.0.0',
+    author='Kohinoor Shrestha, Rahul Shah',
+    author_email='kohinoor.shrestha@deerwalk.edu.np ,rahul.shah@deerwalk.edu.np',
+    packages=find_packages(),
+    install_requires=[]
+)
